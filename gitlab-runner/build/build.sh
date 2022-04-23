474797779817852e202973d6c5124ac2ccff6ab3
Dietrich Zinsou <dietrich.zinsou@hpe.com>
2022-04-23 11:37:32 +0200

    adding appconfig

--- gitlab-runner/build/build.sh
+++ gitlab-runner/build/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Clean and create appconfig archive
-#rm -rf ./kubedirector/appconfig.tgz
-#cd ./kubedirector && tar czf appconfig.tgz appconfig/ && cd -
-#mv ./kubedirector/appconfig.tgz .
+rm -rf ./kubedirector/appconfig.tgz
+cd ./kubedirector && tar czf appconfig.tgz appconfig/ && cd -
+mv ./kubedirector/appconfig.tgz .
 
 # Build and push Docker image
 docker build -t dzinsouhpe/gitlab-runner:python3.9.7 .
@@ -13,4 +13,4 @@ docker tag dzinsouhpe/gitlab-runner:python3.9.7 dzinsouhpe/gitlab-runner:python3
 docker push dzinsouhpe/gitlab-runner:python3
 
 # Delete temporary archive
-#rm -rf ./appconfig.tgz
+rm -rf ./appconfig.tgz
